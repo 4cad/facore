97311084d4b911458543678efb8abce77985a7a5
4cad <dslh.4cad@gmail.com>
2016-06-29 11:04:58 +0000

    Rename test filenames to mirror the compilation units they are testing.

--- compile_tests.sh
+++ compile_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++11 test/TestMain.cpp test/TestDeterministicAutomaton.cpp test/TestNonDeterministicAutomaton.cpp test/TestDRegularLanguage.cpp -I src/
+g++ -std=c++11 test/TestMain.cpp test/TestDAutomaton.cpp test/TestNAutomaton.cpp test/TestDRegularLanguage.cpp -I src/
